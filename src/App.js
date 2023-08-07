e9c74199f9657761ed40bf6f271cd3d0c9f0940f
zahrabola <95353397+zahrabola@users.noreply.github.com>
2023-08-07 22:25:23 +0100

    Update tip

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="tip-contain">
+        <label>Total Bill</label>
+        <input type="text" placeholder={"0.00"} />
+        <label>Tip</label>
+        <input type="text" placeholder={"0.00"} />
+        <div className="summary">
+        summary
+      </div>
+      <div className="result">
+        result
+      </div>
+      </div>
+    
     </div>
   );
 }
