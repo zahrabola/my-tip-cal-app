08e4d19985d4cfa570160d52c3c79025225d1231
zahrabola <95353397+zahrabola@users.noreply.github.com>
2023-08-08 21:54:26 +0100

    Update

--- src/App.js
+++ src/App.js
@@ -1,21 +1,31 @@
 import "./App.css";
+import {useState} from "react";
+const [bill, setBill] = useState(initialState:'');
 
 function App() {
   return (
     <div className="App">
       <div className="tip-contain">
         <label>Total Bill</label>
-        <input type="text" placeholder={"0.00"} />
+        <input type="text" placeholder={"0.00"} value={bill} 
+        onChange={event => setBill(event.target.value)} />
         <label>Tip</label>
         <input type="text" placeholder={"0.00"} />
         <div className="summary">
-        summary
+        <div className="split">
+          <label>Split</label>
+        <div className="split-control">
+          <button> - </button>
+        <span> 1 </span>
+          <button> + </button>
+        </div>
+        </div>
+        <div className="result">
+        <label>Split Total</label>
+        <span>200.00 </span>
       </div>
-      <div className="result">
-        result
       </div>
       </div>
-    
     </div>
   );
 }
